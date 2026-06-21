12889cf62afee000d97eccf939f976cebad8b47f
Premanshu Pandey <premanshuppandey@gmail.com>
2026-06-21 16:59:05 +0530

    Remove v2 call (#23)

--- assets/build/blog-app.asset.php
+++ assets/build/blog-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '2b871c5ce673c86efde7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '1385cf42e0d9637e80a2');
